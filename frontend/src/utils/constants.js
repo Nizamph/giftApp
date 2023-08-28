da957c38620db054f66641f28d282dffb4543135
Nizamph <116118003+Nizamph@users.noreply.github.com>
2023-08-28 20:23:29 +0530

    updated the url

--- frontend/src/utils/constants.js
+++ frontend/src/utils/constants.js
@@ -4,29 +4,35 @@ import cart from '../icons/cart.png';
 import myOrders from '../icons/myOrders.png';
 import addProduct from '../icons/addproduct.png';
 import gift from '../icons/giftIcon.png';
-export const REGISTER = 'http://localhost:4000/api/auth/register';
-export const LOGIN = 'http://localhost:4000/api/auth/login';
-export const ADD_PRODUCT = 'http://localhost:4000/api/products/addProducts';
+export const REGISTER = 'https://gift-app-qtlk.onrender.com/api/auth/register';
+export const LOGIN = 'https://gift-app-qtlk.onrender.com/api/auth/login';
+export const ADD_PRODUCT =
+  'https://gift-app-qtlk.onrender.com/api/products/addProducts';
 export const GET_MY_GIFTS =
-  'http://localhost:4000/api/products/admin/getMyProducts';
+  'https://gift-app-qtlk.onrender.com/api/products/admin/getMyProducts';
 export const GET_ALL_GIFTS_TO_ADMIN =
-  'http://localhost:4000/api/products/admin/getProducts';
+  'https://gift-app-qtlk.onrender.com/api/products/admin/getProducts';
 export const GET_ALL_GIFTS_TO_USER =
-  'http://localhost:4000/api/products/user/getProducts';
-export const ADD_ITEMS_TO_CART = 'http://localhost:4000/api/cart/addItems';
+  'https://gift-app-qtlk.onrender.com/api/products/user/getProducts';
+export const ADD_ITEMS_TO_CART =
+  'https://gift-app-qtlk.onrender.com/api/cart/addItems';
 export const GET_ALL_ITEMS_TO_CART =
-  'http://localhost:4000/api/cart/getCartItems';
-export const DELETE_CART = 'http://localhost:4000/api/cart/deleteCart';
-export const UPDATE_CART = 'http://localhost:4000/api/cart/updateCart';
-export const PLACE_ORDER = 'http://localhost:4000/api/order/placeOrder';
-export const CLEAR_CART = 'http://localhost:4000/api/cart/clearCart';
+  'https://gift-app-qtlk.onrender.com/api/cart/getCartItems';
+export const DELETE_CART =
+  'https://gift-app-qtlk.onrender.com/api/cart/deleteCart';
+export const UPDATE_CART =
+  'https://gift-app-qtlk.onrender.com/api/cart/updateCart';
+export const PLACE_ORDER =
+  'https://gift-app-qtlk.onrender.com/api/order/placeOrder';
+export const CLEAR_CART =
+  'https://gift-app-qtlk.onrender.com/api/cart/clearCart';
 export const GET_ORDER_TO_USER =
-  'http://localhost:4000/api/order/getOrderToUser';
+  'https://gift-app-qtlk.onrender.com/api/order/getOrderToUser';
 export const GET_ORERS_TO_ADMIN =
-  'http://localhost:4000/api/order/getOrderToAdmin';
+  'https://gift-app-qtlk.onrender.com/api/order/getOrderToAdmin';
 
 export const UPDATE_ORDER_STATUS =
-  'http://localhost:4000/api/order/updateOrderStatus';
+  'https://gift-app-qtlk.onrender.com/api/order/updateOrderStatus';
 export const ADMIN_SIDEBAR_CONTENT = [
   {
     name: 'Admin Dashboard',
